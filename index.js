a9a7de00b225c2ac1dfb36ca1ad2a6c1c4943b81
Rebecca Strauss <rstrauss127@gmail.com>
2019-03-21 16:41:57 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,11 @@
+class BoardMember {
+  constructor(name, homeState, training) {
+    this.name = name;
+    this.homeState = homeState;
+    this.traing = training;
+  }
+
+  veto {
+    return "No, I must disagree";
+  }
+}
