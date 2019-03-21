aef6ae6aac1db6dd07cb98ce8b508b1a3cb85f6a
Rebecca Strauss <rstrauss127@gmail.com>
2019-03-21 16:49:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ class BoardMember {
   constructor(name, homeState, training) {
     this.name = name;
     this.homeState = homeState;
-    this.traing = training;
+    this.training = training;
   }
 }
