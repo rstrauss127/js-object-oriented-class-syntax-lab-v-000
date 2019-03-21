e4e078845b34c052713d140161eb3e7ecdc2bb06
Rebecca Strauss <rstrauss127@gmail.com>
2019-03-21 16:52:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ class BoardMember {
   }
 
   releasePressStatement() {
-    return "You will see great things from Scuber";
+    return "You will see great things from Scuber.";
   }
 
   sayHi() {
